e7fa800549cc1944b8548db41ce1588a1a0f8caf
Adam <1815936@cegepoutaouais.qc.ca>
2023-05-08 10:08:39 -0400

    Ajout de commentaires

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from Classe_Etudiant import *
+# Première instance de la classe Etudiant
 E1 = Etudiant()
 E1.Surnom = "Has"
 
 Etudiant.nbEtudiants+=1
+# Deuxième instance de la classe Etudiant
 E2 = Etudiant()
 Etudiant.nbEtudiants+=1
 Etudiant.list_Etudiants.append(E2)
